99cb5ede0b6f51854f56ad99a244f477e2f9a37e
maarjaroots <38770030+maarjaroots@users.noreply.github.com>
2022-03-11 12:25:47 +0200

    Tailwind configuration (#6)
    * Tailwind config file
    
    * Configuration for tailwind ui

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,21 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div className="bg-white">
+      <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
+        <div className="text-center">
+          <h2 className="text-base font-semibold text-indigo-600 tracking-wide uppercase">
+            Blog
+          </h2>
+          <p className="mt-1 text-4xl font-extrabold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl">
+            devs.travel
+          </p>
+          <p className="max-w-xl mt-5 mx-auto text-xl text-gray-500">
+            displaying content soon
+          </p>
+        </div>
+      </div>
+    </div>
+  )
 }
